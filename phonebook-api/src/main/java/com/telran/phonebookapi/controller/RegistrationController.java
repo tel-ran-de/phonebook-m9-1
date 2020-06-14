85673ffe905df1433c0b1cd83c0b669e61d84695
AliviaVi <alivia0408@gmail.com>
2020-06-14 03:22:43 +0200

    Added emailConfirm to Registration Controller, email Sender To user Service. Email properties. Created new Service EmailSender.

--- phonebook-api/src/main/java/com/telran/phonebookapi/controller/RegistrationController.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/controller/RegistrationController.java
@@ -1,10 +1,13 @@
 package com.telran.phonebookapi.controller;
 
 import com.telran.phonebookapi.model.User;
+import com.telran.phonebookapi.service.EmailSenderService;
 import com.telran.phonebookapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,12 +16,25 @@ public class RegistrationController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    EmailSenderService emailSenderService;
 
     @PostMapping("/registration")
     public ResponseEntity<?> addUser(@RequestBody User user) {
         if (!userService.saveUser(user)) {
             return ResponseEntity.badRequest().body("user already exist");
         }
+
         return (ResponseEntity<?>) ResponseEntity.ok();
+    }
+
+    @GetMapping("/confirmation/{token}")
+    public boolean emailConfirmation(@PathVariable String token) {
+        boolean isActivated = userService.activateUser(token);
+
+        if (isActivated) {
+            return true;
+        }
+        return false;
     }
 }
