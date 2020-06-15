0a4a7dae5d526839031aa959da7499650ec7fa87
Artem <art.telran@yandex.ru>
2020-06-15 18:34:27 +0200

    add content, proof privileg

--- phonebook-ui/src/app/app.module.ts
+++ phonebook-ui/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
+import { PendingComponent } from './pending/pending.component';
+import { ActivationComponent } from './activation/activation.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { RecoveryComponent } from './recovery/recovery.component';
+import { NewPasswortComponent } from './new-passwort/new-passwort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    PendingComponent,
+    ActivationComponent,
+    ConfirmationComponent,
+    RecoveryComponent,
+    NewPasswortComponent
   ],
   imports: [
     BrowserModule,
---
+++ phonebook-ui/src/app/new-passwort/new-passwort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-passwort',
+  templateUrl: './new-passwort.component.html',
+  styleUrls: ['./new-passwort.component.css']
+})
+export class NewPasswortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
