718c98d3f1f772d66b08d3e4285aae709cb27719
Liliya Garipova <lilygarif@gmail.com>
2020-06-21 15:22:24 +0200

    renames components

---
+++ phonebook-ui/src/app/activation/activation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-confirmation',
+  templateUrl: './activation.component.html',
+  styleUrls: ['./activation.component.css']
+})
+export class ActivationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- phonebook-ui/src/app/app-routing.module.ts
+++ phonebook-ui/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RegistrationComponent} from "./registration/registration.component";
-import {RegistrationPendingComponent} from "./registration-pending/registration-pending.component";
-import {RegistrationConfirmationComponent} from "./registration-confirmation/registration-confirmation.component";
+import {PendingComponent} from "./pending/pending.component";
+import {ActivationComponent} from "./activation/activation.component";
 
 
 const routes: Routes = [
-  // {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '', redirectTo: '/registration', pathMatch: 'full'},
   {path: 'registration', component: RegistrationComponent},
-  {path: 'registration-pending', component: RegistrationPendingComponent},
-  {path: 'registration-confirmation', component: RegistrationConfirmationComponent},
+  {path: 'pending', component: PendingComponent},
+  {path: 'activation', component: ActivationComponent},
 ];
 
 @NgModule({
