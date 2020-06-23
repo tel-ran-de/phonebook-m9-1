0dc0c1f2cdc50e284eaaa76f25788996d7580534
AliviaVi <alivia0408@gmail.com>
2020-06-23 13:06:45 +0200

    last one

--- phonebook-api/src/main/java/com/telran/phonebookapi/model/ConfirmationToken.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/model/ConfirmationToken.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
-@Entity(name = "ConfirmationToken")
+@Entity
 @Getter
 @NoArgsConstructor
 public class ConfirmationToken {
