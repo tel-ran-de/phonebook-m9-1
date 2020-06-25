50bddcdebd165e57a2df1f8473ac3a188822f8d7
Artem <art.telran@yandex.ru>
2020-06-25 20:58:41 +0200

    add Angular Material take5

--- phonebook-ui/src/app/app.module.ts
+++ phonebook-ui/src/app/app.module.ts
@@ -1,19 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {  MatButtonModule } from  '@angular/material/button';
+
+import {MatCardModule} from '@angular/material/card';
+import { MaterialModule } from './material.module';
+import { PendingPageComponent } from './pending-page/pending-page.component';
+import { LoginComponent } from './login/login.component';
+import { NewPasswordPageComponent } from './new-password-page/new-password-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    PendingPageComponent,
+    LoginComponent,
+    NewPasswordPageComponent,
+
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MaterialModule,
+    MatCardModule
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
