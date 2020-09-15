46393d09b0539b370c3fb5d85ce0287a7cde9c23
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-09-15 10:15:51 +0200

    fixes !!localStorage.getItem("auth-token") to localStorage.getItem(TOKEN_KEY) === null removed google analytics param from angular.json

--- phonebook-ui/src/app/service/auth.guard.ts
+++ phonebook-ui/src/app/service/auth.guard.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 
+const TOKEN_KEY = 'auth-token';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,11 +16,10 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (!!localStorage.getItem("auth-token"))
-      return true;
-    else {
+    if (localStorage.getItem(TOKEN_KEY) === null) {
       this.router.navigate(['user/login']);
       return false;
-    }
+    } else
+      return true;
   }
 }
