996e62cd6fcbfa3ec84b6c0266f2a4a0a661a9ad
AR1988 <37342319+AR1988@users.noreply.github.com>
2020-09-18 16:03:02 +0200

    Bug-2 fixed (#27)
    * bux fixed

--- phonebook-ui/src/app/registration/registration.component.ts
+++ phonebook-ui/src/app/registration/registration.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
 import {ConfirmedValidator} from "./confirmed.validator";
 import {UserService} from "../service/user.service";
 import {HttpErrorResponse} from "@angular/common/http";
-import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-registration',
@@ -18,6 +17,7 @@ export class RegistrationComponent implements OnInit {
   angForm: FormGroup;
   loading: boolean;
   error: string;
+  errorMessage: string;
 
   constructor(private fb: FormBuilder,
               private router: Router,
@@ -41,31 +41,29 @@ export class RegistrationComponent implements OnInit {
   }
 
   onSubmit() {
-    // @ts-ignore
     this.loading = true;
+    this.errorMessage = '';
+
     this.userService.newUserRegistration(this.angForm.value)
       .subscribe(
-        data => {
+        () => {
           this.router.navigate(['user/activate-email']);
         },
         error => {
-          // @ts-ignore
-          this.error = handleError(error);
-          this.loading = false;
-        }
-      )
-
-    function handleError(error: HttpErrorResponse) {
-      if (error.error instanceof ErrorEvent) {
-        // network error
-        console.error(`No internet connection`);
-      } else {
-        // the response may contain hints of what went wrong
-        console.error(`Error code: ${error.status}`);
-      }
-      // user facing error message
-      return throwError(`Something bad happened; please try again later.`);
-    }
+          this.errorMessage = this.errorHandle(error);
+        },
+        () => this.loading = false);
   }
 
+  private errorHandle(error: HttpErrorResponse): string {
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent)
+      return 'No internet connection';
+    else errorMessage = error.error.message;
+
+    if (errorMessage === null || !errorMessage)
+      errorMessage = 'Error code: ' + error.status
+        + '. If you have this error again, please contact us: support@phone-book.com'
+    return errorMessage;
+  }
 }
