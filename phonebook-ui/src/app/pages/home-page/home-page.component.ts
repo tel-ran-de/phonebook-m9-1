47ffd1caca323b1f13100b6e37501049412dd687
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-09-18 12:14:57 +0200

    removed unused statement

--- phonebook-ui/src/app/pages/home-page/home-page.component.ts
+++ phonebook-ui/src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {TokenStorageService} from "../../service/tokenHandle/token-storage.service";
 
 @Component({
   selector: 'app-home-page',
@@ -8,18 +6,10 @@ import {TokenStorageService} from "../../service/tokenHandle/token-storage.servi
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  value: any
-  title = 'Welcome';
 
-  constructor(private tokenStorageService: TokenStorageService,
-              private router: Router) {
+  constructor() {
   }
 
   ngOnInit() {
-  }
-
-  logout() {
-    this.tokenStorageService.signOut();
-    this.router.navigate(['../user/login']).then();
   }
 }
