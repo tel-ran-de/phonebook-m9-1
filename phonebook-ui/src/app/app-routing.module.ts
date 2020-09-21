cb4150869db452c3c58778b962e8838659f88849
AR1988 <37342319+AR1988@users.noreply.github.com>
2020-09-21 19:03:06 +0200

    init user detail page (#34)

--- phonebook-ui/src/app/app-routing.module.ts
+++ phonebook-ui/src/app/app-routing.module.ts
@@ -7,30 +7,41 @@ import {ForgotPasswordComponent} from "./forgot-password/forgot-password.compone
 import {PasswordRecoveryComponent} from "./password-recovery/password-recovery.component";
 import {LoginComponent} from "./login/login.component";
 import {AuthGuard} from "./service/auth.guard";
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {ContactsComponent} from "./pages/contacts/contacts.component";
+import {HomePageComponent} from "./pages/root_page/home-page/home-page.component";
+import {ContactsComponent} from "./pages/root_page/contacts_page/contacts/contacts.component";
+import {AccountPasswordComponent} from "./pages/root_page/account_page/account-password/account-password.component";
+import {AccountComponent} from "./pages/root_page/account_page/account/account.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'contacts', pathMatch: 'full'},
+
   {path: 'user/login', component: LoginComponent},
   {path: 'user/registration', component: RegistrationComponent},
   {path: 'user/activate-email', component: ActivateEmailComponent},
   {path: 'user/activation/:token', component: ActivationComponent},
   {path: 'user/forgot-password', component: ForgotPasswordComponent},
   {path: 'user/password-recovery/:token', component: PasswordRecoveryComponent},
+
   {
     path: '', component: HomePageComponent,
     children: [
       {
         path: 'contacts', component: ContactsComponent
-      }
+      },
+      {path: 'account', redirectTo: 'account/password'},
+      {
+        path: "account", component: AccountComponent, children: [
+          {path: "password", component: AccountPasswordComponent},
+        ]
+      },
     ], canActivate: [AuthGuard]
   },
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 }
