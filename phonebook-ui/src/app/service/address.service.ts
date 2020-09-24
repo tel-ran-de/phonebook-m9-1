cc8c1760face55b2a8d586f7f4c7f6188e682aaa
AR1988 <AudiTT2012>
2020-09-25 00:08:41 +0200

    init

--- phonebook-ui/src/app/service/address.service.ts
+++ phonebook-ui/src/app/service/address.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Address} from "../model/address";
+import {Phone} from "../model/phone";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,8 @@ import {Address} from "../model/address";
 export class AddressService {
 
   private readonly basePath = '/api/address';
+  addresses: Observable<Address[]>;
+  private contactId: number;
 
   constructor(private http: HttpClient) {
   }
@@ -26,5 +30,16 @@ export class AddressService {
 
   editById(addressId: number ,address: Address) {
     return this.http.put<Address>(`${this.basePath}/${addressId}`, address);
+  }
+
+  getAllAddressesByContactId(contactId: number) {
+    this.contactId = contactId;
+    if (!this.addresses)
+      this.reload();
+    return this.addresses;
+  }
+
+  reload(): void {
+    this.addresses = this.http.get<Address[]>(`${this.basePath}/${(this.contactId)}/all`);
   }
 }
--- phonebook-ui/src/app/service/phone.service.ts
+++ phonebook-ui/src/app/service/phone.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Email} from "../model/email";
 import {Phone} from "../model/phone";
+import {Contact} from "../model/contact";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,10 @@ import {Phone} from "../model/phone";
 export class PhoneService {
 
   private readonly basePath = '/api/phone';
+
+  phones: Observable<Phone[]>;
+  private contactId: number;
+
 
   constructor(private http: HttpClient) {
   }
@@ -27,5 +33,16 @@ export class PhoneService {
 
   editById(phoneId: number, phone: Phone) {
     return this.http.put<Phone>(`${this.basePath}/${phoneId}`, phone);
+  }
+
+  getAllPhonesByContactId(contactId: number) {
+    this.contactId = contactId;
+    if (!this.phones)
+      this.reload();
+    return this.phones;
+  }
+
+  reload(): void {
+    this.phones = this.http.get<Phone[]>(`${this.basePath}/${(this.contactId)}/all`);
   }
 }
