9678fd38c8a2da043eeae0d8cc2433a9aa09634f
bolokadze <bolokadze@gmail.com>
2020-09-29 20:13:11 +0200

    add test user

--- phonebook-api/src/main/java/com/telran/phonebookapi/config/TestUserRunner.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/config/TestUserRunner.java
@@ -3,10 +3,12 @@ package com.telran.phonebookapi.config;
 import com.telran.phonebookapi.exception.UserAlreadyExistsException;
 import com.telran.phonebookapi.model.User;
 import com.telran.phonebookapi.persistance.IUserRepository;
+import io.jsonwebtoken.impl.AbstractTextCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import static com.telran.phonebookapi.service.UserService.USER_ALREADY_EXISTS;
@@ -14,6 +16,7 @@ import static com.telran.phonebookapi.service.UserService.USER_ALREADY_EXISTS;
 @Profile(value = "dev")
 @Component
 public class TestUserRunner implements ApplicationRunner {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Value("${com.telran.testUser.testEmail}")
     String testEmail;
     @Value("${com.telran.testUser.testPassword}")
@@ -21,8 +24,9 @@ public class TestUserRunner implements ApplicationRunner {
     final
     IUserRepository userRepository;
 
-    public TestUserRunner(IUserRepository userRepository) {
+    public TestUserRunner(IUserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -30,7 +34,8 @@ public class TestUserRunner implements ApplicationRunner {
         if (userRepository.findById(testEmail).isPresent()) {
             throw new UserAlreadyExistsException(USER_ALREADY_EXISTS);
         } else {
-            User user = new User(testEmail, testPassword);
+            
+            User user = new User(testEmail, bCryptPasswordEncoder.encode(testPassword));
             user.setActive(true);
             userRepository.save(user);
         }
