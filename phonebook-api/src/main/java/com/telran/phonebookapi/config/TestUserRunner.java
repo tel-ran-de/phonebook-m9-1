117e3b88491c51c9abe0e4ecaf75d47782e68d44
Gulnara <40672445+bolokadze@users.noreply.github.com>
2020-09-30 22:18:15 +0200

    fix testUser bug (#53)

--- phonebook-api/src/main/java/com/telran/phonebookapi/config/TestUserRunner.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/config/TestUserRunner.java
@@ -1,9 +1,7 @@
 package com.telran.phonebookapi.config;
 
-import com.telran.phonebookapi.exception.UserAlreadyExistsException;
 import com.telran.phonebookapi.model.User;
 import com.telran.phonebookapi.persistance.IUserRepository;
-import io.jsonwebtoken.impl.AbstractTextCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import static com.telran.phonebookapi.service.UserService.USER_ALREADY_EXISTS;
 
 @Profile(value = "dev")
 @Component
@@ -31,9 +28,7 @@ public class TestUserRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        if (userRepository.findById(testEmail).isPresent()) {
-            throw new UserAlreadyExistsException(USER_ALREADY_EXISTS);
-        } else {
+        if (userRepository.findById(testEmail).isEmpty()) {
             
             User user = new User(testEmail, bCryptPasswordEncoder.encode(testPassword));
             user.setActive(true);
