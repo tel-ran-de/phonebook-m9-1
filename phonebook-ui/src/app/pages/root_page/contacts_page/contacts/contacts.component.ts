c654bb5431b88ac7a74c344fd4e8b0956ff358c5
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-10-08 19:58:47 +0200

    Revert "init PB-49 search contact (simple) (#64)"
    This reverts commit ef712c88a8cf6a622450ee65c8e2181b3b64d6fe.

--- phonebook-ui/src/app/pages/root_page/contacts_page/contacts/contacts.component.ts
+++ phonebook-ui/src/app/pages/root_page/contacts_page/contacts/contacts.component.ts
@@ -3,7 +3,6 @@ import {Subscription} from "rxjs";
 import {ContactService} from 'src/app/service/contact.service';
 import {UserService} from 'src/app/service/user.service';
 import {Contact} from 'src/app/model/contact';
-import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-contacts',
@@ -12,72 +11,41 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class ContactsComponent implements OnInit, OnDestroy {
 
-  private getAllContactsSubscription: Subscription;
   private getProfileSubscription: Subscription;
-
+  private getContactsSubscription: Subscription;
   profile: Contact;
-  contactsFromServer: Contact[];
-  contactsDisplay: Contact[];
+  contacts: Contact[];
 
-  searchContactForm: FormGroup;
-
-  constructor(public contactService: ContactService,
-              public userService: UserService,
-              private fb: FormBuilder) {
+  constructor(public contactService: ContactService, public userService: UserService) {
   }
 
   ngOnInit(): void {
     this.getProfile();
     this.reloadContactsList();
-    this.createForm();
-
-    this.searchContactForm.get('searchInput').valueChanges.subscribe(value => {
-      this.contactsDisplay = this.searchContact(value)
-    })
-
     this.contactService.trigger$.subscribe(() => this.reloadContactsList());
   }
 
   private reloadContactsList() {
-    this.getAllContactsSubscription = this.contactService.getAllContacts()
-      .subscribe(contactList => this.callBackGetAllContactOk(contactList));
+    this.getProfileSubscription = this.contactService.getAllContacts()
+      .subscribe(value => this.contacts = value);
   }
 
   getProfile() {
     this.profile = new Contact();
     this.getProfileSubscription = this.contactService.getProfile()
-      .subscribe(profile => this.callBackGetProfileOk(profile));
-  }
-
-  callBackGetProfileOk(value: Contact) {
-    if (!value.firstName)
-      value.firstName = 'No first name'
-    this.profile = value;
-  }
-
-  callBackGetAllContactOk(value: Contact[]) {
-    this.contactsDisplay = value;
-    this.contactsFromServer = value
-  }
-
-  createForm() {
-    this.searchContactForm = this.fb.group({
-      searchInput: []
-    });
-  }
-
-  searchContact(text: string) {
-    return this.contactsFromServer.filter(value => {
-      const term = text.toLowerCase()
-      const contact = value.firstName + value.lastName + value.description
-      return contact.toLowerCase().includes(term);
-    });
+      .subscribe(value => {
+        if (!value.firstName)
+          value.firstName = 'No first name'
+        this.profile = value;
+      });
   }
 
   ngOnDestroy(): void {
     if (this.getProfileSubscription)
       this.getProfileSubscription.unsubscribe();
-    if (this.getAllContactsSubscription)
-      this.getProfileSubscription.unsubscribe();
+
+    if (this.getContactsSubscription)
+      this.getContactsSubscription.unsubscribe();
   }
+
 }
