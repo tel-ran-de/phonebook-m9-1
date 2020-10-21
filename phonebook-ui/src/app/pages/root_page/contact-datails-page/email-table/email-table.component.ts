8ad0aae2d24260ec93906d2a867c5dc34a33de38
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-10-21 11:25:19 +0200

    adds pip-up and unsubscribe subscription

--- phonebook-ui/src/app/pages/root_page/contact-datails-page/email-table/email-table.component.ts
+++ phonebook-ui/src/app/pages/root_page/contact-datails-page/email-table/email-table.component.ts
@@ -1,15 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Email} from "src/app/model/email";
 import {EmailService} from "../../../../service/email.service";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {EmailEditModalComponent} from "../email-edit-modal/email-edit-modal.component";
+import {ToastService} from "../../../../service/toast.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-email-table',
   templateUrl: './email-table.component.html',
   styleUrls: ['./email-table.component.css']
 })
-export class EmailTableComponent implements OnInit {
+export class EmailTableComponent implements OnInit, OnDestroy {
 
   @Input()
   sortedEmailsToDisplay: Email[];
@@ -17,14 +19,17 @@ export class EmailTableComponent implements OnInit {
   reverseSort: boolean;
   sortBy: string;
 
+  removeSubscription: Subscription;
+
   constructor(private modalService: NgbModal,
-              private emailService: EmailService) {
+              private emailService: EmailService,
+              private toastService: ToastService) {
   }
 
   ngOnInit(): void {
   }
 
-  sort(sortBy: string) {
+  sort(sortBy: string): void {
     if (this.sortBy !== sortBy)
       this.reverseSort = false;
 
@@ -37,12 +42,36 @@ export class EmailTableComponent implements OnInit {
       this.sortedEmailsToDisplay.reverse();
   }
 
-  onClickRemove(emailId: number) {
-    this.emailService.removeEmail(emailId);
+  onClickRemove(emailId: number): void {
+    this.removeSubscription = this.emailService.removeEmail(emailId)
+      .subscribe(() => this.callBackOkRemoveEmail(), () => this.callBackErrorRemoveEmail());
   }
 
-  onClickEditEmail(emailToEdit: Email) {
+  callBackOkRemoveEmail(): void {
+    this.toastService.show('Email removed', {
+      classname: 'bg-success text-light',
+      delay: 5_000,
+      id: 'pop-up-success-remove-email'
+    });
+
+    this.emailService.triggerOnReloadEmailList();
+  }
+
+  callBackErrorRemoveEmail(): void {
+    this.toastService.show('Remove email failed', {
+      classname: `bg-danger text-light`,
+      delay: 7_000,
+      id: `pop-up-error-remove-email`
+    });
+  }
+
+  onClickEditEmail(emailToEdit: Email): void {
     const modalRef = this.modalService.open(EmailEditModalComponent);
     modalRef.componentInstance.emailToEdit = emailToEdit;
+  }
+
+  ngOnDestroy(): void {
+    if (this.removeSubscription)
+      this.removeSubscription.unsubscribe();
   }
 }
