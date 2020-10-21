0ab63ed8e7f736a85250cd9617ac59454cec380e
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-10-21 11:41:17 +0200

    adds pip-up and unsubscribe subscription

--- phonebook-ui/src/app/pages/root_page/contact-datails-page/email/email.component.ts
+++ phonebook-ui/src/app/pages/root_page/contact-datails-page/email/email.component.ts
@@ -2,10 +2,10 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {EmailService} from "src/app/service/email.service";
 import {Email} from "src/app/model/email";
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {SubscriptionErrorHandle} from "src/app/service/subscriptionErrorHandle";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {EmailAddModalComponent} from "../email-add-modal/add-email-modal.component";
 import {Subscription} from "rxjs";
+import {SubscriptionErrorHandle} from "../../../../service/subscriptionErrorHandle";
 
 @Component({
   selector: 'app-email',
@@ -22,7 +22,10 @@ export class EmailComponent implements OnInit, OnDestroy {
   searchFormEmail: FormGroup;
   errorMessage: string;
   loading: boolean;
-  private triggerSubscription: Subscription;
+
+  triggerSubscription: Subscription;
+  formSubscription: Subscription;
+  getAllSubscription: Subscription;
 
   constructor(private emailService: EmailService,
               private fb: FormBuilder,
@@ -32,13 +35,12 @@ export class EmailComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.searchFormEmail = this.fb.group({
       searchInput: []
-    })
+    });
 
     this.reloadEmails();
 
-    this.searchFormEmail.get("searchInput").valueChanges.subscribe(searchText => {
-      this.emailsToDisplay = this.search(searchText);
-    });
+    this.formSubscription = this.searchFormEmail.get("searchInput").valueChanges
+      .subscribe(searchText => this.emailsToDisplay = this.search(searchText));
 
     this.triggerSubscription = this.emailService.trigger$
       .subscribe(() => {
@@ -50,39 +52,42 @@ export class EmailComponent implements OnInit, OnDestroy {
   reloadEmails(): void {
     this.loading = true;
 
-    this.emailService.getAllEmailsByContactId(this.contactId)
-      .subscribe(email => {
-        this.callbackOk(email);
-      }, error => {
-        this.callbackError(error)
-      });
+    this.getAllSubscription = this.emailService.getAllEmailsByContactId(this.contactId)
+      .subscribe(email => this.callbackOk(email), error => this.callbackError(error));
   }
 
-  callbackOk(value: Email[]) {
-    this.errorMessage = ''
-    this.loading = false
-    this.emailsFromServer = value
+  callbackOk(value: Email[]): void {
+    this.errorMessage = '';
+    this.loading = false;
+
+    this.emailsFromServer = value;
     this.emailsToDisplay = value;
   }
 
-  callbackError(error: any) {
-    this.errorMessage = SubscriptionErrorHandle(error)
-    this.loading = false
+  callbackError(error: any): void {
+    this.errorMessage = SubscriptionErrorHandle(error);
+
+    this.loading = false;
   }
 
-  search(text: string) {
+  search(text: string): Email[] {
     return this.emailsFromServer.filter(emailItem => {
-      const term = text.toLowerCase()
-      return emailItem.email.toLowerCase().includes(term)
-    })
+      const term = text.toLowerCase();
+      return emailItem.email.toLowerCase().includes(term);
+    });
   }
 
-  openModalAddEmail() {
+  openModalAddEmail(): void {
     const modalRef = this.modalService.open(EmailAddModalComponent);
     modalRef.componentInstance.contactId = this.contactId;
   }
-  
+
   ngOnDestroy(): void {
-    this.triggerSubscription.unsubscribe();
+    if (this.formSubscription)
+      this.formSubscription.unsubscribe();
+    if (this.getAllSubscription)
+      this.getAllSubscription.unsubscribe();
+    if (this.triggerSubscription)
+      this.triggerSubscription.unsubscribe();
   }
 }
