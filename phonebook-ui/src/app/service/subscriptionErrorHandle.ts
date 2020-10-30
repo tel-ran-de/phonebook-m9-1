5459f944f47ff61b91b38e99ad12900523ebde5b
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-10-30 13:08:31 +0100

    fixed subscription error handle

--- phonebook-ui/src/app/service/subscriptionErrorHandle.ts
+++ phonebook-ui/src/app/service/subscriptionErrorHandle.ts
@@ -1,11 +1,9 @@
 import {HttpErrorResponse} from "@angular/common/http";
 
 export function SubscriptionErrorHandle(error: HttpErrorResponse): string {
-
-  if (error.status === 404 || error.status === 400 || error.status === 504)
+  if (!error.error.message && (error.status === 404 || error.status === 400 || error.status === 504))
     return 'Something bad happened, please try again later.';
   if (error.error instanceof ErrorEvent)
     return 'No internet connection';
-
   return error.error.message || 'If you have this error again, please contact us.';
 }
